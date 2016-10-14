ae1a67a7f850534e0102b5e4c60c6eb1ebc24d1f
Paul <paul@noworriesit.co.uk>
2016-10-14 23:01:26 +0100

    added return to site link rather than auto redirect

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,4 +1,4 @@
 <?php
     mail('roy.woodhead@iottransforms.com', $_POST['subject'], $_POST['message']);
 ?>
-<p>Your email has been sent.</p>
+<p>Your email has been sent. <a href="http://iot.nowo.co.uk"><em>Return To IoT Transforms</em></a></p>
