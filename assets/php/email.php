12b7ee809eb2610f23c417f5e53be91cefa9e2ab
Paul <paul@noworriesit.co.uk>
2016-10-16 16:30:31 +0100

    added POST email field

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,4 +1,4 @@
 <?php
-    mail('roy.woodhead@iottransforms.com', $_POST['subject'], $_POST['message']);
+    mail('roy.woodhead@iottransforms.com', $_POST['email'], $_POST['subject'], $_POST['message']);
 ?>
-<p>Your email has been sent. <a href="http://iottransforms.com"><em>Return To IoT Transforms</em></a></p>
+<p>Your email has been sent. <a href="http://iottransforms.com"><h1><em>Return To IoT Transforms</em></h1></a></p>
